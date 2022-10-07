b8246d00e343f42b93c6f22fb481ef980c6148c5
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-10-07 16:16:11 -0700

    added inputs and output potentials. Generator works

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function getRandomInt(max) {
+  return Math.ceil(Math.random() * max)
+}
+
 // Function to generate password
 function generatePassword() {
 // Step 1: How many characters does the user want the password to be?
@@ -35,4 +39,31 @@ function generatePassword() {
   var useSpecialCharacters = confirm("Use Special Characters?")
 
 // Set up formula to randomize the confirmed choices?
+
+//  Structure of potential character outputs
+ var potentialCharacters = []
+if (useUppercase) {
+  potentialCharacters = potentialCharacters.concat(uppercase)
+}
+
+if (useLowercase) {
+  potentialCharacters = potentialCharacters.concat(lowercase)
+}
+
+if (useNumbers) {
+  potentialCharacters = potentialCharacters.concat(numbers)
+}
+
+if (useSpecialCharacters) {
+  potentialCharacters = potentialCharacters.concat(special)
+}
+
+// Create the password of "passwordLength" length
+
+var password = ''
+for (var i = 0; i < passwordLength; i++) {
+  password = password + potentialCharacters[getRandomInt(potentialCharacters.length)]
+}
+return password
+
 }
