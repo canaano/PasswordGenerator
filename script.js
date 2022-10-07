64d17478a6cae95cb16b5eca47c3af8aa3c7e786
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-10-07 15:16:20 -0700

    added password prompt variables

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var lowercase = uppercase.map(letter => letter.toLowerCase())
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,3 +20,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Function to generate password
+function generatePassword() {
+// Step 1: How many characters does the user want the password to be?
+  var passwordLength = prompt("How long would you like your password to be? (8 - 128)", "12")
+// Step 2: Ask if user wants to use uppercase?
+  var useUppercase = confirm('Use uppercase letters?')
+// Step 3: Ask if user wants to use lowercase?
+  var useLowercase = confirm("Use lowercase letters?")
+// Step 4: Ask if user wants to use numbers?
+  var useNumbers = confirm("Use numbers?")
+// Step 5: Ask if user wants to use special characters?
+  var useSpecialCharacters = confirm("Use Special Characters?")
+
+// Set up formula to randomize the confirmed choices?
+}
